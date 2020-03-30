88728248bb45f05899c14bc40e337ce4c96473f3
Igor Kuchin <ikuchin82@mail.ru>
2020-03-30 20:05:34 +0300

    Updating solution.py

--- solution.py
+++ solution.py
@@ -1,4 +1,4 @@
-def find_users(*file_names):
+def find_users(file_name_1, file_name_2):
     """
     This function should receive file names, at least 2.
     And should return list of user_id's that found in both files. List should have unique values.
